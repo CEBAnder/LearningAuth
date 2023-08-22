3c337e2179512e947602758a9a8db05d95dc1e5a
Vsevolod Parshin <v.parshin@dodopizza.com>
2023-08-22 17:37:32 +0300

    Refactor Program.cs

---
+++ LearningAuth.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,69 @@
+using FluentMigrator.Runner;
+using LearningAuth.Contracts.Shared;
+using LearningAuth.Data;
+using LearningAuth.Data.Migrations;
+using LearningAuth.Web.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LearningAuth.Web.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, string? connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentNullException(connectionString);
+        }
+        
+        services.AddSingleton<DbContext>();
+        services.AddSingleton<Database>();
+        services
+            .AddFluentMigratorCore()
+            .ConfigureRunner(rb => rb
+                .AddMySql5()
+                .WithGlobalConnectionString(connectionString)
+                .ScanIn(typeof(Migration_1_AddUserTable).Assembly).For.Migrations());
+
+        return services;
+    }
+    
+    public static IServiceCollection AddAuth(this IServiceCollection services)
+    {
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(
+                options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = AuthOptions.ISSUER,
+                        ValidateAudience = true,
+                        ValidAudience = AuthOptions.AUDIENCE,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true
+                    };
+                })
+            .AddCookie();
+        services
+            .AddAuthorization(
+                options =>
+                {
+                    options.AddPolicy(
+                        Constants.AuthenticationPolicies.CookieAdmin,
+                        policy =>
+                        {
+                            policy.AuthenticationSchemes.Add(CookieAuthenticationDefaults.AuthenticationScheme);
+                            policy.Requirements.Add(
+                                new RolesAuthorizationRequirement(new List<string> { Role.Admin.ToString() }));
+                        });
+                });
+
+        return services;
+    }
+}
--- LearningAuth.Web/Program.cs
+++ LearningAuth.Web/Program.cs
@@ -1,65 +1,21 @@
-using FluentMigrator.Runner;
-using LearningAuth.Contracts.Shared;
 using LearningAuth.Data;
-using LearningAuth.Data.Migrations;
 using LearningAuth.Data.Repositories;
-using LearningAuth.Web;
-using LearningAuth.Web.Models;
+using LearningAuth.Web.Extensions;
 using LearningAuth.Web.Services;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<DbContext>();
-builder.Services.AddSingleton<Database>();
-builder.Services
-    .AddFluentMigratorCore()
-    .ConfigureRunner(rb => rb
-        .AddMySql5()
-        .WithGlobalConnectionString(builder.Configuration.GetConnectionString("SqlConnection"))
-        .ScanIn(typeof(Migration_1_AddUserTable).Assembly).For.Migrations());
+builder.Services.AddDatabase(builder.Configuration.GetConnectionString("SqlConnection"));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 
-builder.Services
-    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(
-        options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = AuthOptions.ISSUER,
-                ValidateAudience = true,
-                ValidAudience = AuthOptions.AUDIENCE,
-                ValidateLifetime = true,
-                IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
-                ValidateIssuerSigningKey = true
-            };
-        })
-    .AddCookie();
-builder.Services
-    .AddAuthorization(
-        options =>
-        {
-            options.AddPolicy(
-                Constants.AuthenticationPolicies.CookieAdmin,
-                policy =>
-                {
-                    policy.AuthenticationSchemes.Add(CookieAuthenticationDefaults.AuthenticationScheme);
-                    policy.Requirements.Add(
-                        new RolesAuthorizationRequirement(new List<string> { Role.Admin.ToString() }));
-                });
-        });
+builder.Services.AddAuth();
 
 var app = builder.Build();
 
