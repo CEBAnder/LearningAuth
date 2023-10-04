58b3ac6fbc93b265046a8387715edef26214c747
Vsevolod Parshin <v.parshin@dodopizza.com>
2023-10-04 22:26:38 +0300

    Add simple unit test.

--- Tests/LearningAuth.Web.Tests.Unit/Services/AccountServiceTests.cs
+++ Tests/LearningAuth.Web.Tests.Unit/Services/AccountServiceTests.cs
@@ -1,6 +1,31 @@
+using LearningAuth.Web.Services;
+using Moq;
+using NUnit.Framework;
+
 namespace LearningAuth.Web.Tests.Unit.Services;
 
 public class AccountServiceTests
 {
-    
+    [Test]
+    public async Task GenerateTokenForUserAsync_ReturnsToken_WhenValidInput()
+    {
+        var accountService = GetAccountServiceMock();
+
+        var token = await accountService.GenerateTokenForUserAsync("name", "password");
+        
+        Assert.That(token, Is.Not.Empty);
+    }
+
+    private IAccountService GetAccountServiceMock()
+    {
+        var mock = new Mock<IAccountService>();
+        mock
+            .Setup(x => x.GenerateTokenForUserAsync(
+                It.IsAny<string>(), 
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync("some_token");
+
+        return mock.Object;
+    }
 }
